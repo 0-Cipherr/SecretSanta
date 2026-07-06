3de70cc8465dffe1cd23b691351c6dc88c2fad45
ExoticSolutions <1exoticeditz@gmail.com>
2026-07-06 10:08:53 -0400

    done

---
+++ src/contracts/Vault.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+contract Vault {
+    function deposit() public {}
+
+    function withdraw() public {}
+
+    function getTotalAssets() public {}
+
+    function getShares() public {}
+
+    function getCurrentStrategy() public {}
+}
